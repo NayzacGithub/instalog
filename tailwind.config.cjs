7486024452240e65a3bb72d9c1df619ff22e4b70
NayzacGithub <github@nayzac.online>
2022-09-01 14:00:15 +0200

    Basic Tailwind Config

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'SF Pro Display', 'Segoe UI', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+};
