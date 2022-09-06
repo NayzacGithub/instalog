ae146d87727cd3af305c414bba873dbaa1ffa9dd
NayzacGithub <github@nayzac.online>
2022-09-07 00:08:43 +0200

    Added github provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,12 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import DiscordProvider from "next-auth/providers/discord";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
+
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -19,12 +21,20 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+
+    }),
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    })
     // ...add more providers here
   ],
+  secret: env.NEXTAUTH_SECRET,
+
+
 };
 
 export default NextAuth(authOptions);
