628e5319a165fd1aab8239fc04559c23dbf37d76
NayzacGithub <github@nayzac.online>
2022-09-07 00:05:57 +0200

    Adding env validation

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,12 +6,15 @@ export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.string().url(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
 });
 
 
 export const clientSchema = z.object({
+  NEXT_PUBLIC_BASE_URL: z.string(),
 });
 
 /**
@@ -21,4 +24,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
+  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
 };
