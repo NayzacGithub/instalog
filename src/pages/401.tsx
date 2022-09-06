0c36f6078df5dc17273cc9dd6bf5ac776ec8470e
NayzacGithub <github@nayzac.online>
2022-09-07 00:05:47 +0200

    Error pages

---
+++ src/pages/401.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import InstaLogLogo from "../components/InstalogLogo";
+
+const Unautherized: NextPage = () => {
+    return (
+        <div className="grid gap-4 place-items-center place-content-center h-screen text-white">
+            <InstaLogLogo className=" h-[150px]" />
+            <h1 className="text-5xl font-mono">401</h1>
+            <p className="text-3xl font-mono text-gray-400">Unauthorized</p>
+        </div>
+    );
+}
+
+export default Unautherized;
