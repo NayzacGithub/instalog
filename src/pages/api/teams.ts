57fedd163c53a6b93e04fde97981916cb26756cf
NayzacGithub <github@nayzac.online>
2022-09-07 00:06:46 +0200

    teams API handler

---
+++ src/pages/api/teams.ts
@@ -0,0 +1,79 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
+import slugify from "slugify";
+import { prisma } from "../../server/db/client";
+import { authOptions } from "./auth/[...nextauth]";
+
+const teams = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    const _method = req.method;
+    const params = req.query;
+    const session = await unstable_getServerSession(req, res, authOptions);
+
+    switch (_method) {
+        case "GET":
+            if (session?.user?.id) {
+                if (params.teamSlug) {
+                    const team = await prisma.team.findFirst({
+                        where: {
+                            slug: params.teamSlug as string,
+                            members: {
+                                some: {
+                                    userId: session.user.id
+                                }
+                            }
+                        },
+                    });
+                    res.status(200).json({ message: 'success', data: team });
+                }
+                else {
+                    const teams = await prisma.team.findMany({
+                        where: {
+                            members: {
+                                some: {
+                                    userId: session.user.id
+                                }
+                            }
+                        }
+                    });
+                    res.status(200).json({ message: 'Success', data: teams, count: teams?.length || 0 });
+                }
+            } else {
+                res.status(401).json({ message: 'Unauthorized' });
+            }
+            break;
+        case "POST":
+            if (session?.user?.id) {
+                const newTeam = await prisma.team.create({
+                    data: {
+                        name: req.body.name,
+                        slug: req.body.slug,
+                        members: {
+                            create: {
+                                userId: session.user.id
+                            }
+                        },
+                        Invertory: {
+                            create: {
+                                name: `${req.body.name}'s invertory`,
+                                slug: slugify(req.body.name, { lower: true, strict: true }) + '-invertory',
+                            }
+                        }
+
+                    }
+                });
+                res.status(200).json({ message: 'Success', data: newTeam });
+
+            } else {
+                res.status(401).json({ message: 'Unauthorized' });
+            }
+            break;
+
+        default:
+            res.status(401).json({ message: "Unauthorized" });
+            break;
+    }
+
+}
+
+
+export default teams;
